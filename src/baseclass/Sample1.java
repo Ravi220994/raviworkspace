e42b205d8fe474d037b10c8c385af322a77bb932
Ravi220994 <ravi220994@gmail.com>
2021-03-10 21:59:20 +0530

    code on ravi

--- src/baseclass/Sample1.java
+++ src/baseclass/Sample1.java
@@ -1,10 +1,14 @@
 package baseclass;
 
+import org.openqa.selenium.By;
 
 
-public class Sample1 {
+public class Sample1  {
 
 	public static void main(String[] args) {
+		String s="ravi";
+		System.out.println(s);
+		
 		
 		
 	}
